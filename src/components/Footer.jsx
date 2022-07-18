cf9afce2fbd2d248a4249075728360e9ccbc687b
Roman Pohribniak <roman.pohribniak@gmail.com>
2022-07-18 20:50:31 +0300

    deploy

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return <section className="footer">
-        <div className="footer__bar">
-            <h3 className="footr__bar--info">
-            The site was made in Ukraine by <br /> <a href="https://www.instagram.com/pohribniak.roman">Roman Pohribniak</a> & <a href="https://www.instagram.com/galact1c_d151nt3grat0r/">Nazarii Yahotin</a> 
-            </h3>
-        </div>
-    </section>
-}
--- src/components/SocialLink.jsx
+++
@@ -1,3 +0,0 @@
-export default function SocialLink({icon,link}){
-    return <a href={link} className="organizations__links--icon" rel="noreferrer" target="_blank" style={{backgroundImage:`url(${icon})`,}}></a>
-}
